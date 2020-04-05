29d0cf9e72c1f44c51102eb61b8729592f5ce9cf
John Doe <johndoe@example.com>
2020-04-05 09:30:32 -0700

    first commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+  description = "Name of the Resource Group"
+}
+
+variable "location" {
+  description = "Enter Region in which all resources in this example should be created."
+}
+
+variable "servername" {
+  description = "Enter name of the first server being built in this RG."
+}
